da5f4d32bebdf2e47562c8152e25042b88f43089
Ryan Glasnapp <Ryan.Glasnapp@gmail.com>
2002-02-05 01:49:56 +0000

    added includes

--- newserver/server.c
+++ newserver/server.c
@@ -2,8 +2,21 @@
 #include <pthread.h>	// pthread ops
 #include "listen.h"
 #include "backend.h"
+#include "common.h"
+#include "universe.h"
+#include "msgqueue.h"
+#include "player_interaction.h"
+#include "sysop_interaction.h"
+#include "hashtable.h"
+#include "planet.h"
+#include "serveractions.h"
+#include "shipinfo.h"
+#include "config.h"
 
-int main(){
+
+
+int main()
+{
 	pthread_t console_id, listen_id, backend_id;
 
 	init_listen_thread(&listen_id);
@@ -13,4 +26,3 @@ int main(){
 	printf("main thread exiting...\n");
 	return 0;
 }
-
