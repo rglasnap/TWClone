2ef9e212a9b0439c2c1c428843875370551d0eb3
Ryan Glasnapp <Ryan.Glasnapp@gmail.com>
2002-05-10 20:09:09 +0000

    Beginnings of a timing thread

--- source/server.c
+++ source/server.c
@@ -35,6 +35,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "serveractions.h"
 #include "shipinfo.h"
 #include "config.h"
+#include "maint.h"
 
 struct sector **sectors;
 int sectorcount;
