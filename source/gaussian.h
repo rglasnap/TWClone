c6ed9ef781bbf6d523a82f94bfffcc099f09c936
Ryan Glasnapp <Ryan.Glasnapp@gmail.com>
2003-10-29 23:20:11 +0000

    Removed these redundant files...(it's already in box_muller.c)

--- source/gaussian.h
+++
@@ -1,7 +0,0 @@
-//Courtesy of Hernan Lobert
-//Email: hlobert@yahoo.com.br
-//http://www.wilmott.com/messageview.cfm?catid=4&threadid=6390
-//As of date Oct 16 2003.
-
-double NormX(double mi, double sigma)
-double Unif(unsigned i)
